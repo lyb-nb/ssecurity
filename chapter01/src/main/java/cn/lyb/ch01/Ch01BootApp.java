66ae128232ff62b0f9afe2deae36ca1f338de67f
zjc <zjc-kim@gmail.com>
2019-08-06 23:39:43 +0800

    Initial commit

---
+++ chapter01/src/main/java/cn/lyb/ch01/Ch01BootApp.java
@@ -0,0 +1,15 @@
+package cn.lyb.ch01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author zhengjianc
+ * @Date 2019/8/6 23:31
+ */
+@SpringBootApplication
+public class Ch01BootApp {
+    public static void main(String[] args) {
+        SpringApplication.run(Ch01BootApp.class, args);
+    }
+}
---
+++ chapter01/src/main/java/cn/lyb/ch01/controller/HelloController.java
@@ -0,0 +1,16 @@
+package cn.lyb.ch01.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author zhengjianc
+ * @Date 2019/8/6 23:31
+ */
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello(){
+        return "+++Hello Spring Boot+++";
+    }
+}
